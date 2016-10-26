1ae77127997bc426951acdabaeba1f749dbd1a9a
Robin Crorie <robin@appease.biz>
2016-10-25 21:36:52 -0400

    Added base twitter auth project structure

---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'TwitterAuth' do
+  # Comment this line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+  pod 'Fabric'
+  pod 'TwitterKit'
+
+  # Pods for TwitterAuth
+
+  target 'TwitterAuthTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'TwitterAuthUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ TwitterAuth/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  TwitterAuth
+//
+//  Created by Robin Crorie on 25/10/2016.
+//  Copyright © 2016 AppEase Ltd. All rights reserved.
+//
+
+import UIKit
+import TwitterKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let logInButton = TWTRLogInButton { (session, error) in
+            if let unwrappedSession = session {
+                let alert = UIAlertController(title: "Logged In",
+                    message: "User \(unwrappedSession.userName) has logged in",
+                    preferredStyle: UIAlertControllerStyle.Alert
+                )
+                alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+                self.presentViewController(alert, animated: true, completion: nil)
+            } else {
+                NSLog("Login error: %@", error!.localizedDescription);
+            }
+        }
+        
+        // TODO: Change where the log in button is positioned in your view
+        logInButton.center = self.view.center
+        self.view.addSubview(logInButton)
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
